bb55d07d341bac4cc11e81f0ba69410f44c6bc7e
Eduardo Zielke <ezielke@brasilseg.com.br>
2023-09-27 02:08:25 -0300

    adicionado validador de data

--- model/pessoa.go
+++ model/pessoa.go
@@ -1,11 +1,29 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Date string
+
+func (d *Date) UnmarshalJSON(b []byte) error {
+	s := string(b)
+
+	t, err := time.Parse(`"2006-01-02"`, s)
+	if err != nil {
+		return err
+	}
+
+	*d = Date(t.Format("2006-01-02"))
+	return nil
+}
 
 type Pessoa struct {
 	ID         primitive.ObjectID `json:"id" bson:"_id"`
 	Nome       string             `json:"nome" validate:"required,min=1,max=32"`
 	CpfCnpj    string             `json:"cpfCnpj" bson:"cpf_cnpj" validate:"required,max=14"`
-	Nascimento string             `json:"nascimento" bson:"nascimento" validate:"required"`
+	Nascimento Date               `json:"nascimento" bson:"nascimento" validate:"required"`
 	Seguros    []string           `json:"seguros" bson:"seguros" validate:"omitempty,min=1,dive,max=32"`
 }
